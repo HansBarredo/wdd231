2495b8c7cdb120fcc237e1d21336d3aa962382a7
Hans Barredo <hansit.barredo@gmail.com>
2025-04-10 03:52:50 +0800

    chamber discover page

--- chamber/scripts/discover.js
+++ chamber/scripts/discover.js
@@ -61,4 +61,35 @@ function displayPlacesCards(places) {
         
         container.appendChild(placeCard);
     });
+
+    const visitDialog = document.getElementById("visit-message");
+    const lastVisit = localStorage.getItem("lastVisit");
+    const now = new Date();
+    let message = "";
+  
+    if (!lastVisit) {
+      message = "Welcome! Let us know if you have any questions.";
+    } else {
+      const previous = new Date(lastVisit);
+      const diffTime = now - previous;
+      const diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24));
+  
+      if (diffDays === 0) {
+        message = "Back so soon! Awesome!";
+      } else if (diffDays === 1) {
+        message = "You last visited 1 day ago.";
+      } else {
+        message = `You last visited ${diffDays} days ago.`;
+      }
+    }
+  
+    visitDialog.textContent = message;
+    visitDialog.showModal();
+  
+
+    visitDialog.addEventListener("click", () => {
+      visitDialog.close();
+    });
+
+    localStorage.setItem("lastVisit", now.toISOString());
 }
