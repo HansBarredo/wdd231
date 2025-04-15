7c5a21fb1647d827f2b636baa6317109b0bcd33c
Hans Barredo <hansit.barredo@gmail.com>
2025-04-15 20:21:09 +0800

    js edit

--- project/scripts/donate.js
+++ project/scripts/donate.js
@@ -2,39 +2,47 @@ import { initNavigation } from './navToggle.js';
 
 initNavigation();
 
-const visitDialog = document.getElementById("visit-message");
-    const lastVisit = localStorage.getItem("lastVisit");
-    const now = new Date();
-    let message = "";
-  
-    if (!lastVisit) {
-      message = "Welcome! Ready to make a donation?";
+function handleVisitMessage() {
+  const visitDialog = document.getElementById("visit-message");
+  if (!visitDialog) return;
+
+  const lastVisit = localStorage.getItem("lastVisit");
+  const now = new Date();
+  let message = "";
+
+  if (!lastVisit) {
+    message = "Welcome! ready to make a donation?";
+  } else {
+    const previous = new Date(lastVisit);
+    const diffTime = now - previous;
+    const diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24));
+
+    if (diffDays === 0) {
+      message = "Back so soon! Awesome!";
+    } else if (diffDays === 1) {
+      message = "You last visited 1 day ago.";
     } else {
-      const previous = new Date(lastVisit);
-      const diffTime = now - previous;
-      const diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24));
-  
-      if (diffDays === 0) {
-        message = "Back so soon! Awesome!";
-      } else if (diffDays === 1) {
-        message = "You last visited 1 day ago.";
-      } else {
-        message = `You last visited ${diffDays} days ago.`;
-      }
+      message = `You last visited ${diffDays} days ago.`;
     }
-  
-    visitDialog.textContent = message;
-    visitDialog.showModal();
-  
+  }
 
-    visitDialog.addEventListener("click", () => {
-      visitDialog.close();
-    });
+  visitDialog.textContent = message;
+  visitDialog.showModal();
 
-    localStorage.setItem("lastVisit", now.toISOString());
+  visitDialog.addEventListener("click", () => {
+    visitDialog.close();
+  });
+
+  localStorage.setItem("lastVisit", now.toISOString());
+}
+
+
+handleVisitMessage();
+
 
     let oLastModif = new Date(document.lastModified);
 
-modified.innerHTML = `Last Modification: <span class="last-mod">${oLastModif.toLocaleString()}</span>`;
-
-document.getElementById('copyright-year').textContent = new Date().getFullYear();
+    modified.innerHTML = `Last Modification: <span class="last-mod">${oLastModif.toLocaleString()}</span>`;
+    
+    document.getElementById('copyright-year').textContent = new Date().getFullYear();
+    
