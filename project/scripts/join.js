7c619009836714541b26cf749b1a064e7c1e55e0
Hans Barredo <hansit.barredo@gmail.com>
2025-04-15 20:13:13 +0800

    modules

--- project/scripts/join.js
+++ project/scripts/join.js
@@ -1,41 +1,6 @@
-const mainnav = document.querySelector('#navigation');
-const hambutton = document.querySelector('#hamburger');
-const navList = document.querySelector('#nav-list');
+import { initNavigation } from './navToggle.js';
 
-document.addEventListener("DOMContentLoaded", () => {
-  mainnav.classList.remove("show");
-  hambutton.classList.remove("show", "hide");
-  navList.classList.add("hide");
-});
-
-
-hambutton.addEventListener('click', () => {
-  const isShown = mainnav.classList.contains('show');
-
-  if (isShown) {
-    mainnav.classList.remove('show');
-    hambutton.classList.remove('show');
-    hambutton.classList.add('hide');
-    navList.classList.add('hide');
-  } else {
-    mainnav.classList.add('show');
-    hambutton.classList.remove('hide');
-    hambutton.classList.add('show');
-    navList.classList.remove('hide');
-    setTimeout(() => {
-      navList.classList.add('none'); 
-    }, 3000);
-  }
-});
-
-document.addEventListener('click', e => {
-  if (!mainnav.contains(e.target) && e.target !== hambutton) {
-    mainnav.classList.remove('show');
-    hambutton.classList.remove('show');
-    hambutton.classList.add('hide');
-    navList.classList.add('hide');
-  }
-});
+initNavigation();
 
 document.addEventListener("DOMContentLoaded", () => {
     fetchMemberships();
--- project/scripts/summary.js
+++ project/scripts/summary.js
@@ -1,50 +1,6 @@
-const mainnav = document.querySelector('#navigation');
-const hambutton = document.querySelector('#hamburger');
-const navList = document.querySelector('#nav-list');
+import { initNavigation } from './navToggle.js';
 
-document.addEventListener("DOMContentLoaded", () => {
-  mainnav.classList.remove("show");
-  hambutton.classList.remove("show", "hide");
-  navList.classList.add("hide");
-});
-
-
-hambutton.addEventListener('click', () => {
-  const isShown = mainnav.classList.contains('show');
-
-  if (isShown) {
-    mainnav.classList.remove('show');
-    hambutton.classList.remove('show');
-    hambutton.classList.add('hide');
-    navList.classList.add('hide');
-  } else {
-    mainnav.classList.add('show');
-    hambutton.classList.remove('hide');
-    hambutton.classList.add('show');
-    navList.classList.remove('hide');
-    setTimeout(() => {
-      navList.classList.add('none'); 
-    }, 3000);
-  }
-});
-
-document.addEventListener('click', e => {
-  if (!mainnav.contains(e.target) && e.target !== hambutton) {
-    mainnav.classList.remove('show');
-    hambutton.classList.remove('show');
-    hambutton.classList.add('hide');
-    navList.classList.add('hide');
-  }
-});
-
-document.addEventListener('click', e => {
-  if (!mainnav.contains(e.target) && e.target !== hambutton) {
-    mainnav.classList.remove('show');
-    hambutton.classList.remove('show');
-    hambutton.classList.add('hide');
-    navList.classList.add('hide');
-  }
-});
+initNavigation();
 
 const myInfo = new URLSearchParams(window.location.search);
 const firstName = myInfo.get('fname') || 'Valued Member';
